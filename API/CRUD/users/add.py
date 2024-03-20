4975be1a50cae292b638f72343bfaf6f0ef47ae2
HarryEddward <he0780070@gmail.com>
2024-03-20 13:34:03 +0100

    update and add requirements.txt

--- API/CRUD/users/add.py
+++ API/CRUD/users/add.py
@@ -8,6 +8,26 @@ class data(BaseModel):
     reservas: dict
     info: dict
 
+class info(BaseModel):
+    pass
+
+'''
+STRUCTURE:
+
+{
+    id,
+    data: {
+        reservas: {},
+        info: {
+            username: str,
+            number_phone: str,
+            password: str
+        }
+    }
+    
+}
+'''
+
 
 def add_user(data: dict):
     pass
