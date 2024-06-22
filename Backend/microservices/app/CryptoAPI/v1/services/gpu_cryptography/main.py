d62483827ff3fdb17df0264a2ff10b402d70ea48
HarryEddward <he0780070@gmail.com>
2024-06-22 16:03:35 +0200

    changes

--- Backend/microservices/app/CryptoAPI/v1/services/gpu_cryptography/main.py
+++ Backend/microservices/app/CryptoAPI/v1/services/gpu_cryptography/main.py
@@ -1,1 +1,12 @@
 from Backend.microservices.app.CryptoAPI.v1.dependencies.CUDA_AES_main.AES import AES
+from dotenv import load_dotenv, find_dotenv
+
+
+find_dotenv()
+
+
+def encrypt(text: str) -> str:
+
+    text = text.encode('utf-8')
+    compute = AES()
+    AES.encrypt_gpu()
