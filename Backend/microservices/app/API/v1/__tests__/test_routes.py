7b1a2a28fb3c91463730877b43959632ce5f8e72
HarryEddward <he0780070@gmail.com>
2024-08-08 16:27:12 +0200

    changes

--- Backend/microservices/app/API/v1/__tests__/test_routes.py
+++
@@ -1,9 +0,0 @@
-'''from fastapi.testclient import TestClient
-import pytest
-import httpx
-
-from Backend.microservices.app.API.v1.server_fastapi import app
-
-client: function = TestClient(app)
-
-def test'''
