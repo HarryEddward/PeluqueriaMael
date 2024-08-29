f873fac6a05bd30295c7487543679cf2eedae60a
HarryEddward <he0780070@gmail.com>
2024-08-29 22:03:19 +0200

    changes

---
+++ Backend/microservices/app/CryptoAPI/v1/middlewares/handleError.py
@@ -0,0 +1,21 @@
+# handleMiddleware.py
+
+from fastapi import Request, FastAPI
+from fastapi.responses import JSONResponse
+from starlette.middleware.base import BaseHTTPMiddleware
+
+class ErrorMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        try:
+            response = await call_next(request)
+            return response
+        except Exception as exc:
+            # Puedes personalizar el manejo de errores aquí
+            return JSONResponse(
+                status_code=500,
+                content={
+                    "info": "Ha ocurrido un error interno en el servidor.",
+                    "status": "ok",
+                    "type": "GLOBAL_ERROR"
+                }
+            )
